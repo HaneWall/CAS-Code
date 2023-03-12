3c15672dba3d7907c4725a63cd9d22735a76c278
HaneWall <48854946+HaneWall@users.noreply.github.com>
2023-03-12 15:19:33 +0100

    bugfix

--- generate/generate.jl
+++ generate/generate.jl
@@ -266,7 +266,7 @@ function sidebar_code(book_model)
     <div class="sidebar-about">
     <br>
     <img src="$(path_to_asset(LOGO_FILE))" style="margin-left:1em; width: 230px; height: auto; display: inline">
-	<img src="$(path_to_asset("julia-logo-color.svg"))" style="margin-left:3em; width: 80px; height: auto; display: inline">
+	<img src="$(path_to_asset("julia-logo.svg"))" style="margin-left:3em; width: 80px; height: auto; display: inline">
     <div style="font-weight: bold; margin-bottom: 0.5em">
 	$(href("$(SLASH_PREPATH)/semesters/", TERM))
 	<span style="opacity: 0.6;">|
